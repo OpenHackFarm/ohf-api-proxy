757746a76d323da2a38067155e9efc6a72683b94
yan <a0726h77@gmail.com>
2018-08-25 00:58:24 +0800

    Get sensor data

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, jsonify, Response, request
 from flask_cors import CORS
 import requests
 import json
+from dateutil import parser
+import datetime
 
 from config import *
 
@@ -43,10 +45,24 @@ def cwb_special_weather(city=None):
             return jsonify(resultJSON)
         else:
             resultDict = {}
-            resultDict['WeatherAlarm'] = [] 
-            return resultDict 
+            resultDict['WeatherAlarm'] = []
+            return resultDict
     else:
         return jsonify(resultJSON)
+
+
+@app.route('/iot/data/<dev_id>/latest', methods=['GET'])
+def latest_sensor_data(dev_id=None):
+    url = 'http://%s:%s/query?pretty=true&db=%s&q=SELECT * FROM "test" WHERE "id"=\'%s\' ORDER BY DESC LIMIT 1' % (INFLUXDB_HOST, INFLUXDB_PORT, INFLUXDB_DB, dev_id)
+    print(url)
+    r = requests.get(url)
+    data = r.json()['results'][0]['series'][0]
+
+    ret_data = dict(zip(data['columns'], data['values'][0]))
+    ret_data['time'] = (parser.parse(ret_data['time']) + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
+
+    return ret_data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host=HOST, port=PORT)
